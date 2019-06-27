1655ef30c12fec4537ea48013b8d1ffd11d922d4
Uladzislau Klachkou <uladzislau.klachkou@altoros.com>
2019-06-27 16:18:21 +0300

    Add scrollspy, styling.

--- DiscoveryOne/src/index.js
+++ DiscoveryOne/src/index.js
@@ -1,1 +1,16 @@
 import './styles/main.scss';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const elems = document.querySelectorAll('.scrollspy');
+  const instances = M.ScrollSpy.init(elems, {});
+
+  const headerRef = document.querySelector('.Banner .navbar-fixed');
+
+  document.addEventListener('scroll', (e) => {
+    if (window.pageYOffset > 0) {
+      headerRef.classList.add('black-background')
+    } else if (headerRef.classList.contains('black-background')) {
+      headerRef.classList.remove('black-background')
+    }
+  })
+});
